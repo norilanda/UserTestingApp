bcc9f96da140756a4f367f51ede142380b0ada48
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-11-24 18:16:01 +0200

    Add services interfaces

---
+++ backend/src/UserTestingApp.BLL/Services/TestService.cs
@@ -0,0 +1,33 @@
+﻿using UserTestingApp.BLL.Interfaces;
+using UserTestingApp.Common.DTOs.Answer;
+using UserTestingApp.Common.DTOs.Test;
+
+namespace UserTestingApp.BLL.Services;
+
+public class TestService : ITestService
+{
+    public Task<IEnumerable<TestDto>> GetAssignedTestsForUserAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TestDto>> GetCompletedTestsForUserAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<IncompleteTestDto>> GetIncompleteTestsForUserAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TestWithTasksDto> GetTestWithTasksByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double> PassTestAsync(long testId, long userId, List<AnswerDto> answers)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/src/UserTestingApp.BLL/Services/TokenService.cs
@@ -0,0 +1,11 @@
+﻿using UserTestingApp.BLL.Interfaces;
+
+namespace UserTestingApp.BLL.Services;
+
+public class TokenService : ITokenService
+{
+    public string GenerateAccessToken(long userId)
+    {
+        throw new NotImplementedException();
+    }
+}
