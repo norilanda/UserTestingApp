00a7bf2b5ad6081800c770b216c81736dedb6f56
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-11-26 10:56:13 +0200

    Add unit tests for password hashing methods

---
+++ backend/tests/Common.Tests.Unit/PasswordHasherHelperTests/HashPasswordTests.cs
@@ -0,0 +1,43 @@
+﻿using UserTestingApp.Common.Security;
+using Xunit;
+
+namespace Common.Tests.Unit.PasswordHasherHelperTests;
+
+public class HashPasswordTests : PasswordHasherHelperBaseTests
+{
+    [Fact]
+    public void HashPassword_WhenSamePasswordAndDifferentSalt_ShouldGenerateDifferentHash()
+    {
+        // Arrange
+        var password1 = "my_password@1";
+        var password2 = password1;
+
+        var salt1 = GenerateSalt();
+        var salt2 = GenerateSalt();
+
+        // Act
+        var passwordHash1 = PasswordHasherHelper.HashPassword(password1, salt1);
+        var passwordHash2 = PasswordHasherHelper.HashPassword(password2, salt2);
+
+        // Assert
+        Assert.NotEqual(passwordHash1, passwordHash2);
+    }
+
+    [Fact]
+    public void HashPassword_WhenSamePasswordAndSameSalt_ShouldGenerateSameHash()
+    {
+        // Arrange
+        var password1 = "my_password@1";
+        var password2 = password1;
+
+        var salt1 = GenerateSalt();
+        var salt2 = salt1;
+
+        // Act
+        var passwordHash1 = PasswordHasherHelper.HashPassword(password1, salt1);
+        var passwordHash2 = PasswordHasherHelper.HashPassword(password2, salt2);
+
+        // Assert
+        Assert.Equal(passwordHash1, passwordHash2);
+    }
+}
