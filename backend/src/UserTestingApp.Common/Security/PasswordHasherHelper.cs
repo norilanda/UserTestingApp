28cd8d8b0b03e401a23151dcdc00e1853f3cfaeb
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-11-26 11:06:40 +0200

    Implement password hashing logic

--- backend/src/UserTestingApp.Common/Security/PasswordHasherHelper.cs
+++ backend/src/UserTestingApp.Common/Security/PasswordHasherHelper.cs
@@ -1,19 +1,39 @@
-﻿namespace UserTestingApp.Common.Security;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UserTestingApp.Common.Security;
 
 public static class PasswordHasherHelper
 {
+    private const int _keySize = 64;
+    private const int _iterations = 10000;
+    private static readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
+
     public static byte[] GenerateSalt()
     {
-        throw new NotImplementedException();
+        return RandomNumberGenerator.GetBytes(_keySize);
     }
 
     public static string HashPassword(string password, byte[] salt)
     {
-        throw new NotImplementedException();
+        var hash = GenerateHash(password, salt);
+
+        return Convert.ToHexString(hash);
+    }
+
+    private static byte[] GenerateHash(string password, byte[] salt)
+    {
+        return Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            _iterations,
+            _hashAlgorithm,
+            _keySize);
     }
 
     public static bool VerifyPassword(string password, string hash, byte[] salt)
     {
-        throw new NotImplementedException();
+        var incomePasswordHash = GenerateHash(password, salt);
+        return CryptographicOperations.FixedTimeEquals(incomePasswordHash, Convert.FromHexString(hash));
     }
 }
